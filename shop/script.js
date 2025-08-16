3ca37c88f089435e9e520a9d31cac6748958be30
SAJJAD <122774486+snoou@users.noreply.github.com>
2025-08-16 22:33:25 +0330

    Add files via upload

---
+++ shop/script.js
@@ -0,0 +1,7 @@
+const openSearch = document.getElementById('openSearch');
+openSearch.addEventListener('click', () => {
+      mobileSearch.classList.toggle('hidden');
+      
+     
+      }
+    );
